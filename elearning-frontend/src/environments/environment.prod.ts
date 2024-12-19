0a335ff07ab2dc60aa83c3f8535cf0aadd6c0302
Ibraheem-360 <ibrahimshittu007@gmail.com>
2024-12-19 18:03:14 -0500

    in

--- elearning-frontend/src/environments/environment.prod.ts
+++ elearning-frontend/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: true,
     url: 'https://islam-learning.click',
-    apiUrl: 'https://islam-learning.click/api',
-    google: 'https://islam-learning.click/oauth2/authorization/google'
+    apiUrl: 'https://api.islam-learning.click/api',
+    google: 'https://api.islam-learning.click/oauth2/authorization/google'
 };
