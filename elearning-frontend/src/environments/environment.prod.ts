cb793ec734240135f78a75015b6fd0bcd18c5284
Ibraheem-360 <ibrahimshittu007@gmail.com>
2024-12-19 09:00:45 -0500

    frontend config

--- elearning-frontend/src/environments/environment.prod.ts
+++ elearning-frontend/src/environments/environment.prod.ts
@@ -1,6 +1,11 @@
 export const environment = {
     production: true,
-    url: 'http://localhost:9000',
-    apiUrl: 'http://localhost:9000/api',
-    google: 'http://localhost:9000/oauth2/authorization/google'
+    // url: 'http://localhost:9000',
+    // apiUrl: 'http://localhost:9000/api',
+    // google: 'http://localhost:9000/oauth2/authorization/google'
+
+    url: 'http://learn-islam.run.place',
+    apiUrl: 'http://learn-islam.run.place/api',
+    google: 'http://learn-islam.run.place/oauth2/authorization/google'
 };
+
